9643c4a7edaa797b4dc987f2a486370d38ed23e3
pedro tarcizio pereira galdino <devpedrotarcizio@gmail.com>
2026-05-05 10:30:13 -0300

    SDL3 added in the project

--- main.cpp
+++ main.cpp
@@ -1,3 +1,51 @@
+#include <SDL3/SDL.h>
+
+
 int main() {
+	const int width{ 1920 };
+	const int height{ 991 };
+
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		SDL_Log("failed");
+		return -1;
+	};
+
+	SDL_Window* window;
+	window = SDL_CreateWindow("CPU Renderer", width, height, SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
+
+	SDL_Renderer* render;
+	render = SDL_CreateRenderer(window, 0);
+
+	SDL_Texture* texture;
+	// Usando pixelformat ABGR pois assim ele usa como RGBA
+	texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_ABGR32, SDL_TEXTUREACCESS_STREAMING, width, height);
+
+
+	bool isRunning{ true };
+
+	SDL_Event event;
+	while (isRunning) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				isRunning = false;
+			}
+
+		}
+
+		SDL_UpdateTexture(texture, nullptr, nullptr, width * sizeof(uint32_t));
+
+		SDL_RenderClear(render);
+		SDL_RenderTexture(render, texture, nullptr, nullptr);
+		SDL_RenderPresent(render);
+
+		SDL_Delay(16);
+	}
+
+
+
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(render);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 }
